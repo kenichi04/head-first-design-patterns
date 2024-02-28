e58fc87a3b3d7fe820b1c8b8f9a642e0aa0eb49f
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-02-28 22:10:56 +0900

    p21 > 振る舞いを動的に変更

---
+++ src/section1/FlyRocketPowerd.java
@@ -0,0 +1,8 @@
+package section1;
+
+public class FlyRocketPowerd implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("ロケットで飛んでいます！");
+    }
+}
---
+++ src/section1/ModelDuck.java
@@ -0,0 +1,12 @@
+package section1;
+
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+    @Override
+    public void display() {
+        System.out.println("模型のカモです");
+    }
+}
