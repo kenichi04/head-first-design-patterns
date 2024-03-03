1a55d78fb768be9e929738e27258521e14766ec5
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-03-03 17:25:42 +0900

    Beverageにサイズ追加

--- src/section3/decorator/Beverage.java
+++ src/section3/decorator/Beverage.java
@@ -1,10 +1,20 @@
 package section3.decorator;
 
 public abstract class Beverage {
+    public enum Size { TALL, GRANDE, VENTI };
+    Size size = Size.TALL;
     String description = "不明な飲み物";
 
     public String getDescription() {
         return description;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Size getSize() {
+        return this.size;
     }
 
     public abstract double cost();
--- src/section3/decorator/Soy.java
+++ src/section3/decorator/Soy.java
@@ -12,6 +12,10 @@ public class Soy extends CondimentDecorator {
 
     @Override
     public double cost() {
-        return beverage.cost() + 0.15;
+        double cost = beverage.cost();
+        if (beverage.getSize() == Size.TALL) cost += 0.10;
+        else if (beverage.getSize() == Size.GRANDE) cost += 0.15;
+        else if (beverage.getSize() == Size.VENTI) cost += 0.20;
+        return cost;
     }
 }
