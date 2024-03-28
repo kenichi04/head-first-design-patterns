516bf1df58d98d839329e23f69a893e0ac5aaa85
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-03-28 22:27:51 +0900

    p205 > GarageDoorOpenCommand関連追加

---
+++ src/section6/command/GarageDoorOpenCommand.java
@@ -0,0 +1,14 @@
+package section6.command;
+
+public class GarageDoorOpenCommand implements Command {
+    GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    @Override
+    public void execute() {
+        garageDoor.up();
+    }
+}
--- src/section6/command/RemoteControlTest.java
+++ src/section6/command/RemoteControlTest.java
@@ -5,8 +5,12 @@ public class RemoteControlTest {
         SimpleRemoteControl remote = new SimpleRemoteControl();
         Light light = new Light();
         LightOnCommand lightOn = new LightOnCommand(light);
+        GarageDoor garageDoor = new GarageDoor();
+        GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
 
         remote.setCommand(lightOn);
+        remote.buttonWasPressed();
+        remote.setCommand(garageOpen);
         remote.buttonWasPressed();
     }
 }
