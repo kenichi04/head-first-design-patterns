0da9dcd9e8bf22f8403e324ad40c382a9c76207e
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-04-07 08:21:09 +0900

    p216 > ラムダ式へ変更

--- src/section6/command/RemoteLoader.java
+++ src/section6/command/RemoteLoader.java
@@ -6,20 +6,20 @@ public class RemoteLoader {
 
         Light livingRoomLight = new Light("リビングルーム");
         Light kitchenLight = new Light("キッチン");
-//        GarageDoor garageDoor = new GarageDoor("ガレージ")
         Stereo stereo = new Stereo("リビングルーム");
 
-        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+//        LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+//        LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
         LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
         LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
-
+git 
         StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
         StereoOffWithCDCommand stereoOffWithCD = new StereoOffWithCDCommand(stereo);
 
-        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+//        remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+        // Commandインターフェースが1つの抽象メソッドのみ持つため、ラムダ式へ置き換え可
+        remoteControl.setCommand(0, () -> livingRoomLight.on(), () -> livingRoomLight.off());
         remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
-//        remoteControl.setCommand(2, garageDoorOpen, garageDoorClose);
         remoteControl.setCommand(3, stereoOnWithCD, stereoOffWithCD);
 
         System.out.println(remoteControl);
